f88e124d976a53f97080eeca55cd8a05b2b9bc0b
eminkaan9999-creator <eminkaan9999@gmail.com>
2026-09-16 16:33:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,svg,json}'],
         runtimeCaching: [
           {
-            urlPattern: /^https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/.*$/i,
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/npm\/@mediapipe\/tasks-vision\/.*$/i,
             handler: 'CacheFirst',
             options: { cacheName: 'mediapipe-wasm', expiration: { maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 30 } }
           },
           {
-            urlPattern: /^https://storage.googleapis.com/mediapipe-models/.*$/i,
+            urlPattern: /^https:\/\/storage\.googleapis\.com\/mediapipe-models\/.*$/i,
             handler: 'CacheFirst',
             options: { cacheName: 'mediapipe-model', expiration: { maxEntries: 3, maxAgeSeconds: 60 * 60 * 24 * 30 } }
           }
